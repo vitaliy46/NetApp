95bd8749864d3962cf1914c14516f0ef1ba65f2f
Vitaliy <rss9@mail.ru>
2016-10-26 21:18:06 +0300

    Добавьте файлы проекта.

---
+++ NetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NetApp.Startup))]
+namespace NetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
