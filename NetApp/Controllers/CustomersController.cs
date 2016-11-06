d0a4bfab89ea4353c6b81fc42c9d74e0cdd5d981
Vitaliy <rss9@mail.ru>
2016-11-06 22:52:48 +0300

    fix1

--- NetApp/Controllers/CustomersController.cs
+++ NetApp/Controllers/CustomersController.cs
@@ -18,7 +18,7 @@ namespace NetApp.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            return View(db.Customers.ToList());
+            return View(db.Customers.Include("CustomersContacts").ToList());
         }
 
         // GET: Customers/Details/5
--- NetApp/Models/DataModels.cs
+++ NetApp/Models/DataModels.cs
@@ -12,19 +12,28 @@ namespace NetApp.Models
         public long Inn { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
+
+        public List<CustomerContact> CustomersContacts { get; set; }
     }
 
     public class CustomerContact
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public List<CustomerContactPhone> CustomersContactsPhones { get; set; }
     }
 
     public class CustomerContactPhone
     {
         public int Id { get; set; }
-        public int CustomerId { get; set; }
         public string Phone { get; set; }
+
+        public int CustomerContactId { get; set; }
+        public CustomerContact CustomerContact { get; set; }
     }
 
     public class Supplier
