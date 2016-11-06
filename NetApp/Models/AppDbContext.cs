5b64d602bdb0fd957685b02a9eb90f1081512162
Vitaliy <rss9@mail.ru>
2016-11-06 18:58:11 +0300

    add Customer and Supplier controllers

---
+++ NetApp/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NetApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerContact> CustomersContacts { get; set; }
+        public DbSet<CustomerContactPhone> CustomersContactsPhones { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ NetApp/Models/DataModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Inn { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class CustomerContact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CustomerContactPhone
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
